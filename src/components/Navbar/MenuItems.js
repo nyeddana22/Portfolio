7fd1d3d879882c984a2f57e9907e6ae43c87fb0d
nyeddana22 <nyeddana@ucdavis.edu>
2022-08-04 15:03:03 -0700

    Add existing files to github

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        label: 'About Me',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        label: 'Skills',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        label: 'Projects',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        label: 'Blog',
+        url: '#',
+        cName: 'nav-links',
+    }
+]
