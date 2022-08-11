c7f758b4d50d21db2ead791d1f6b82f6e8333282
nyeddana22 <nyeddana@ucdavis.edu>
2022-08-10 22:26:41 -0700

    Completed blog

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
+    
     <div className="App">
       <Navbar />
       <Routes>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import Scroll from 'react-scroll'
+const ScrollLink = Scroll.ScrollLink
 
 class Navbar extends Component {
   state = { clicked: false };
@@ -8,20 +10,31 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="NavbarItems">
-          <Link to='/'> <h1 className="navbar-logo"> NY</h1></Link>
-      
+        <Link to="/">
+          <h1 className="navbar-logo"> NY</h1>
+        </Link>
+
         <ul className="nav-menu">
           <li className="nav-links">
-            <Link to='/aboutme'>About Me</Link>
-            </li>
-            <li className="nav-links">
-            <Link to='/skills'>Skills</Link>
-            </li>
-            <li className="nav-links">
-            <Link to='/projects'>Projects</Link>
-            </li>
-            <li className="nav-links">
-            <Link to='/blog'>Blog</Link>
+            <Link to="/aboutme">About Me</Link>
+          </li>
+          <li className="nav-links">
+            <Link to="/skills">Skills</Link>
+          </li>
+          <li className="nav-links">
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li className="nav-links">
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li className="resume-button">
+            <a
+              href="https://drive.google.com/file/d/1GrNvqEaOuWnufwEQy-dnJxO_2r4_nKAr/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Resume
+            </a>
           </li>
         </ul>
       </nav>
